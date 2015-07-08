457bb923a850f4ac7fa91dc57a637098fc674e06
tammykan <100508025@nccu.edu.tw>
2015-07-09 00:55:00 +0800

    Update CanNode.java

--- CanNode.java
+++ CanNode.java
@@ -3,168 +3,129 @@ package tw.nccu.edu.dht;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Random;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class CanNode extends NodeBase implements Runnable {
+public abstract class NodeBase {
+	private String ip = null;
+	private int port = 0;
+	private ServerSocket server;
+	private MulticastSocket ms;
+	InetAddress ia = InetAddress.getByName("224.5.5.5");  // broadcast IP
 
-	Coordinate c = new Coordinate();
-	// Hashtable<String,Coordinate>(); // neighborhood table
-	HashSet<CanNode> neighbour = new HashSet<CanNode>();
-	HashMap<String, String> table = new HashMap<String, String>(); // (key,value) table
-	private static int nodeNum = 0;
-	private static CanNode bootstrap;
-
-	public CanNode(int port, String type) throws IOException {
-		super(port, type);
-		c = null;
-	}
-
-	public CanNode(int port, Coordinate c, String type) throws IOException {
-		super(port, type);
-		this.c = c;
-	}
-
-	public Coordinate getCoordinate() {
-		return c;
-	}
-
-	public void addNeighbours(CanNode node) {
-		neighbour.add(node);
-	}
-
-	public void addTable(Peer peer) {
-		table.put(peer.getKey(), peer.getValue());
-	}
-
-	/*
-	 * public void showNeighbours(){ System.out.println("My Neighbours: ");
-	 * Enumeration<String> e = neighbour.keys(); while(e.hasMoreElements()){
-	 * String s = e.nextElement().toString(); // neighbour's IP Coordinate c =
-	 * (Coordinate) neighbour.get(s); // neighbour's Coordinate
-	 * System.out.println(s); c.showCoordinate(); } }
-	 */
-
-	public void showTable() {
-		System.out.println("My Files: ");
-		for (Object key : table.keySet()) {
-			   System.out.println("Key: " + key.toString() + "\tValue: "+ table.get(key));
-			   }
+	public NodeBase(int port) throws IOException {
+			this.port = port;
+			this.ip = InetAddress.getLocalHost().getHostAddress();
+			ms = createMulticastSocket(port);
+			ms.joinGroup(ia);
 	}
 	
-	public int getRandomPoint() {
-		Random r = new Random();
-		return r.nextInt(10);
+	public void setIp(String ip){
+		this.ip = ip;
 	}
 
-	public void join() throws IOException {
-		if (nodeNum == 0) {
-			System.out.println("Join the first Node.");
-			bootstrap = this;
-			c = new Coordinate(0, 0, 10, 10);
-			nodeNum += 1;
-		} else {
-			nodeNum += 1;
-			System.out.println("Join the node N" + nodeNum);
-			int x = bootstrap.getRandomPoint();
-			int y = bootstrap.getRandomPoint();
-			CanNode node = routeNode(x, y);
-			node.split(this);
-			node.addNeighbours(this);
+	public int getServerPort() {
+		return this.port;
+	}
+
+	public static ServerSocket createSocketServer(int port) {
+		ServerSocket serverSocket = null;
+
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		return serverSocket;
 	}
 
-	public void delete() {
+	private Socket client;
+	private BufferedInputStream in;
+	private BufferedOutputStream out;
 
-		if (nodeNum == 0)
-			System.out.println("Please join the first Node.");
-		else {
-			nodeNum -= 1;
-			System.out.println("Delete the node.");
-		}
-	}
-
-	/* update the neighborhood */
-	public void updateNeighbours() {
-
-	}
-
-	public void updateTable(String key, String value) {
-
-	}
-
-	public void split(CanNode node) throws IOException {
-		if (c.isSplitVertical()) {
-			int x = c.getUpperx();
-			c.splitVertical();
-			node.c = new Coordinate(c.getUpperx(), c.getLowery(), x,
-					c.getUppery()); // set new coordinate
-		} else {
-			int y = c.getUppery();
-			c.splitHorizontal();
-			node.c = new Coordinate(c.getLowerx(), c.getUppery(),
-					c.getUpperx(), y); // set new coordinate
-		}
-	}
-
-	public void insertPeer(Peer peer) throws IOException {
-		int x = peer.HashX(peer.getKey());
-		int y = peer.HashY(peer.getKey());
-		if (c.isContain(x, y)) {
-			addTable(peer); // add peer's information to the node table
-		} else {
-			CanNode node = routeNode(x, y);
-			node.addTable(peer);
-		}
-	}
-
-	public String searchPeer(String key) {
-		Peer peer = new Peer();
-		int x = peer.HashX(key);
-		int y = peer.HashY(key);
-
-		if (c.isContain(x, y)) {
-			if (table.containsKey(key))
-				return table.get(key).toString(); // return value
-			else
-				return ("Failure");
-		} else {
-
-		}
-		return null;
-	}
-
-	public CanNode routeNode(int x, int y) throws IOException {
-		CanNode node = new CanNode(getServerPort(), "M");
-		if (bootstrap.c.isContain(x, y))
-			return bootstrap;
-		else {
-			double distance = Double.MAX_VALUE;
-			Iterator<CanNode> iterator = neighbour.iterator();
-			CanNode temp = new CanNode(getServerPort(), "M");
-			while (iterator.hasNext()) {
-				temp = iterator.next();
-				if (temp.c.distance(x, y) <= distance) {
-					distance = temp.c.distance(x, y);
-					node = temp;
+	public void start() {
+		while (true) {
+			try {
+				synchronized (server) {
+					client = server.accept();
+					System.out.println("Get Connection : InetAddress = " + client.getInetAddress());
 				}
+				client.setSoTimeout(15000);
+				in = new BufferedInputStream(client.getInputStream());
+				out = new BufferedOutputStream(client.getOutputStream());
+				process(in, out);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
-		return node;
 	}
 
-	@Override
-	protected void process(BufferedInputStream input,
-			BufferedOutputStream output) {
-		// TODO Auto-generated method stub
+	abstract protected void process(BufferedInputStream input,
+			BufferedOutputStream output);
 
+	protected void sendMessage(String targetAddress, int targetPort,
+			String message) throws IOException {
+		Socket sink = new Socket();
+		InetSocketAddress isa = new InetSocketAddress(targetAddress, targetPort);
+		System.out.println("Connecting...");
+		try {
+			sink.connect(isa, 10000);
+			BufferedOutputStream out = new BufferedOutputStream(
+					sink.getOutputStream());
+			out.write(message.getBytes());
+			out.close();
+			System.out.println("Successful");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			sink.close();
+		}
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	
+	public static MulticastSocket createMulticastSocket(int port) {
+		MulticastSocket multicastSocket = null;
+		try {
+			multicastSocket = new MulticastSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return multicastSocket;
 	}
 
+	public String receive()
+    {
+    	String str = new String();
+    	try{
+    		byte[] buffer = new byte[1024];
+    		while(true){
+    			DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+    			ms.receive(dp);
+    			str = new String(dp.getData(), 0, dp.getLength());
+    			System.out.println("Node: " + ip + " receive message: " + str);
+    			break;
+    		}
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	}
+    	return str;
+    }
+
+	public void send(String str)
+    {
+    	try{
+    		System.out.println("Node: " + ip + " send message: " + str);
+    		byte[] data = str.getBytes();
+    		DatagramPacket dp = new DatagramPacket(data, data.length, ia, port);
+    		ms.send(dp);
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	}
+    }
 }
+
